ce3f49ea23d48ca8c4d10bc125792ffd0668437e
Ben McMann <bbmcmann@volosys.com>
2019-09-01 12:54:08 -0600

    Add loops instead of ifs. Done

--- SignUp.java
+++ SignUp.java
@@ -5,9 +5,17 @@ public class SignUp{
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
+
         String password, username;
 
+        boolean startsWithVowel = false;
+        boolean hasSpecial = false;
+        boolean correctNumPlace = true;
+
         String[] usernames = {"admin", "person", "jimbob234", "skater_gurl17", "gary"};
+        String[] vowels = {"A", "E", "I", "O", "U"};
+        String[] specialChars = {"!", "#", "$", "&", "*"};
+        String[] numbers = {"0","1","2","3","4","5","6","7","8","9"};
         
 
         System.out.println("WELCOME! Enter your username.");
@@ -29,35 +37,76 @@ public class SignUp{
         System.out.println("Enter the password: ");
         password = input.nextLine();
 
-        if (password.toUpperCase().startsWith("A") || password.toUpperCase().startsWith("E") || password.toUpperCase().startsWith("I") || password.toUpperCase().startsWith("O") || password.toUpperCase().startsWith("U")){
-            System.out.println("Good, it begins with a vowel.");
-
-            if (password.indexOf("!") != -1 || password.indexOf("#") != -1 || password.indexOf("&") != -1 || password.indexOf("$") != -1 || password.indexOf("*") != -1){
-                System.out.println("Good, it has a special character.");
-
-                if (password.indexOf(username) == -1){
-                    System.out.println("Good, it does not have the username.");
-
-                    if (password.substring(0, password.length()-2).indexOf("0") == -1 && password.substring(0, password.length()-2).indexOf("1") == -1 && password.substring(0, password.length()-2).indexOf("2") == -1 && password.substring(0, password.length()-2).indexOf("3") == -1 && password.substring(0, password.length()-2).indexOf("4") == -1 && password.substring(0, password.length()-2).indexOf("5") == -1 && password.substring(0, password.length()-2).indexOf("6") == -1 && password.substring(0, password.length()-2).indexOf("7") == -1 && password.substring(0, password.length()-2).indexOf("8") == -1 && password.substring(0, password.length()-2).indexOf("9") == -1){
-                        System.out.println("Good, the numbers are correct. PASSWORD ACCEPTED");
-                    }
-                    else{
-                        System.out.println("Numbers incorrect.");
-                    }
-                }
-                else{
-                    System.out.println("It has the username.");
-                }
+        for (int i = 0; i < 5; i++){
+            if(password.toUpperCase().startsWith(vowels[i])){
+                startsWithVowel = true;
             }
-            else{
-                System.out.println("Does not have a special character.");
+        }
+        if(!startsWithVowel){
+            System.out.println("Password rejected due to missing vowel.");
+            System.exit(0);
+        }
+
+        for (int i = 0; i < 5; i++){
+            if(password.indexOf(specialChars[i]) != -1){
+                hasSpecial = true;
             }
-
         }
-        else{
-            System.out.println("Does not begin with a vowel.");
-
+        if(!hasSpecial){
+            System.out.println("Password rejected due to missing special char.");
+            System.exit(0);
         }
+
+        if(password.indexOf(username) != -1){
+            System.out.println("Password rejected due to inclusion of username.");
+            System.exit(0);
+        }
+
+        for (int i = 0; i < 9; i++){
+            // System.out.println(password.substring(0, password.length()-2));
+            // System.out.println("Testing: " + numbers[i]);
+            // System.out.println("Index: " + password.substring(0, password.length()-2).indexOf(numbers[i]));
+            
+            if(password.substring(0, password.length()-2).indexOf(numbers[i]) != -1){
+                correctNumPlace = false;
+            }
+        }
+        if(!correctNumPlace){
+            System.out.println("Password rejected due to incorrect number placement.");
+            System.exit(0);
+        }
+
+        System.out.println("Password accepted");
+        
+        // if (password.toUpperCase().startsWith("A") || password.toUpperCase().startsWith("E") || password.toUpperCase().startsWith("I") || password.toUpperCase().startsWith("O") || password.toUpperCase().startsWith("U")){
+        //     System.out.println("Good, it begins with a vowel.");
+
+        //     if (password.indexOf("!") != -1 || password.indexOf("#") != -1 || password.indexOf("&") != -1 || password.indexOf("$") != -1 || password.indexOf("*") != -1){
+        //         System.out.println("Good, it has a special character.");
+
+        //         if (password.indexOf(username) == -1){
+        //             System.out.println("Good, it does not have the username.");
+
+        //             if (password.substring(0, password.length()-2).indexOf("0") == -1 && password.substring(0, password.length()-2).indexOf("1") == -1 && password.substring(0, password.length()-2).indexOf("2") == -1 && password.substring(0, password.length()-2).indexOf("3") == -1 && password.substring(0, password.length()-2).indexOf("4") == -1 && password.substring(0, password.length()-2).indexOf("5") == -1 && password.substring(0, password.length()-2).indexOf("6") == -1 && password.substring(0, password.length()-2).indexOf("7") == -1 && password.substring(0, password.length()-2).indexOf("8") == -1 && password.substring(0, password.length()-2).indexOf("9") == -1){
+        //                 System.out.println("Good, the numbers are correct. PASSWORD ACCEPTED");
+        //             }
+        //             else{
+        //                 System.out.println("Numbers incorrect.");
+        //             }
+        //         }
+        //         else{
+        //             System.out.println("It has the username.");
+        //         }
+        //     }
+        //     else{
+        //         System.out.println("Does not have a special character.");
+        //     }
+
+        // }
+        // else{
+        //     System.out.println("Does not begin with a vowel.");
+
+        // }
     }
 
 }

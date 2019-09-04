ad98adc6eec566e0c05afd536f46e1ef16316e49
Ben McMann <bbmcmann@volosys.com>
2019-09-04 13:08:56 -0600

    Add like 1 if statement...

--- TimeUntil.java
+++ TimeUntil.java
@@ -20,7 +20,6 @@ public class TimeUntil{
 
         futureTime = input.nextLine();
         
-        System.out.println(futureTime);
 
         futureHour = Integer.parseInt(futureTime.substring(0, 2));
         futureMinute = Integer.parseInt(futureTime.substring(3, 5));
@@ -30,10 +29,26 @@ public class TimeUntil{
         System.out.println(currentMinute);
         System.out.println(currentSecond);
         
+        // hourDiff = ldt.minusHours(futureHour);
+        // minuteDiff = ldt.minusMinutes(futureMinute);
+        // secondDiff = ldt.minusSeconds(futureSecond);
+
+       
+        
+        if(!(futureSecond > currentSecond)){
+            currentMinute = currentMinute + 1;
+        }
+
         hourDiff = futureHour - currentHour;
         minuteDiff = futureMinute - currentMinute;
         secondDiff = futureSecond - currentSecond;
+
         
+
+        // System.out.println(hourDiff);
+        // System.out.println(minuteDiff);
+        // System.out.println(secondDiff);
+
         //able to collect data. now need to write code to determine what type of math to do 
     }
 

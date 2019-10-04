e25e1070906fb81dfd95edd6f7f05627495cb672
Ben McMann <bbmcmann@volosys.com>
2019-10-04 12:22:58 -0600

    Initial Commit

---
+++ Farkle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Farkle{
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+    }
+
+
+}

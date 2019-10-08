52e7cdc0dd3d2763f925a6cab5a7992aa5a83dd1
Ben McMann <bbmcmann@volosys.com>
2019-10-08 12:51:34 -0600

    Rolling Function Finish, Works Well

--- Farkle.java
+++ Farkle.java
@@ -11,10 +11,10 @@ public class Farkle{
         int playerOneScore = 0;
         int playerTwoScore = 0;
         int workingScore = 0;
-        int[] p1Roll = {-1,-1,-1,-1,-1,-1};
-        int[] p2Roll = {-1,-1,-1,-1,-1,-1};
+        int[] p1Roll = new int[6];
+        int[] p2Roll = new int[6];
 
-        boolean p1Turn;
+        boolean p1Turn, rolling;
 
         System.out.println("Gather around this flat screen!");
 
@@ -30,9 +30,11 @@ public class Farkle{
 
 
         if(playerOneRoll > playerTwoRoll){
+            System.out.println("Player 1 rolls first");
             p1Turn = true;
         }
         else{
+            System.out.println("Player 2 rolls first");
             p1Turn = false;
         }
 
@@ -41,17 +43,33 @@ public class Farkle{
         while(playerOneScore < 10000 && playerTwoScore < 10000){
 
             if(p1Turn){
+                System.out.println("__________________");
+                System.out.println("PLAYER 1 TURN");
 
                 p1Roll = roll(6);
 
+                for(int i = 0; i < p1Roll.length; i ++){
+                    if(p1Roll[i] > -2){
+                        System.out.println("Die " + (i + 1) + ":" + p1Roll[i]);
+                    }
+                }
+                //while(rolling){
+
+
+                //}
+
+                playerOneScore += 1000;
                 p1Turn = false;
+                System.out.println("__________________");
             }
             else{
-
+                System.out.println("__________________");
+                System.out.println("PLAYER 2 TURN");
                 p2Roll = roll(6);
 
 
                 p1Turn = true;
+                System.out.println("__________________");
             }
 
         }
@@ -61,12 +79,12 @@ public class Farkle{
     public static int[] roll(int times){
         Random random = new Random();
 
-        int[] num = {};
-        for(int i = times; i > 0; i --){
+        int[] num = new int[6];
+        for(int i = times - 1; i > -1; i --){
             num[i] = random.nextInt(6) + 1;
         }
-        for(int i = 6-times; i > 0; i --){
-            num[i] = -1;
+        for(int i = 5-times; i > -1; i --){
+            num[i+times] = -1;
         }
         
         return num;

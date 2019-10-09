d2cef5acd360537204aee413f41b5f5d9931bcd6
Ben McMann <bbmcmann@volosys.com>
2019-10-09 12:32:17 -0600

    Three of a Kind Function, I think

--- Farkle.java
+++ Farkle.java
@@ -13,10 +13,17 @@ public class Farkle{
         int workingScore = 0;
         int[] p1Roll = new int[6];
         int[] p2Roll = new int[6];
-
+        int numRolling = 6;
+        int dieOut;
+        String takeOut = "";
         boolean p1Turn, rolling;
 
         System.out.println("Gather around this flat screen!");
+        System.out.println("DISCLAIMER: This game is absed off of the rules found on wikihow");
+        System.out.println("Go look if you don't know how to play.");
+        System.out.println("Cheating is not allowed because cheating is against te rules. So dont do it.");
+
+
 
         System.out.println("Player 1 rolls first.");
         playerOneRoll = random.nextInt(6)+1;
@@ -38,25 +45,60 @@ public class Farkle{
             p1Turn = false;
         }
 
-
-
         while(playerOneScore < 10000 && playerTwoScore < 10000){
 
             if(p1Turn){
                 System.out.println("__________________");
                 System.out.println("PLAYER 1 TURN");
 
-                p1Roll = roll(6);
+                p1Roll = roll(numRolling);
 
                 for(int i = 0; i < p1Roll.length; i ++){
                     if(p1Roll[i] > -2){
                         System.out.println("Die " + (i + 1) + ":" + p1Roll[i]);
                     }
                 }
-                //while(rolling){
 
+                System.out.println("Do you want to take out any 3 of a kind?(y/n)");
+                takeOut = input.nextLine();
 
-                //}
+                while(takeOut.equals("y")){
+                    System.out.println("Which die? (enter the number ON the die)");
+                    dieOut = input.nextInt();
+
+                    if(dieOut == 1){
+                        playerOneScore += 1000;
+                    }
+                    else{
+                        playerOneScore += dieOut * 100;
+                    }
+
+                    numRolling --;
+                    System.out.println("Do you want to take out any 3 of a kind?(y/n)");
+                    takeOut = input.nextLine();
+                    takeOut = input.nextLine();
+                }
+
+                System.out.println("Do you want to take out any 1's or 5's?(y/n)");
+                takeOut = input.nextLine();
+
+                //only runs if taking out dice
+                while(takeOut.equals("y")){
+                    System.out.println("Which dice? (enter 1 or 5)");
+                    dieOut = input.nextInt();
+
+                    if(dieOut == 1){
+                        playerOneScore += 100;
+                    }
+                    else{
+                        playerOneScore += 50;
+                    }
+
+                    numRolling --;
+                    System.out.println("Do you want to take out any 1's or 5's?(y/n)");
+                    takeOut = input.nextLine();
+                }
+                
 
                 playerOneScore += 1000;
                 p1Turn = false;
@@ -88,5 +130,26 @@ public class Farkle{
         }
         
         return num;
+    }
+
+    public static boolean threeKind(int[] roll, int ammountRolling){
+        int numEqual = 0;
+
+        for(int i = 1; i <= ammountRolling; i++){
+
+            for(int n = 0; n < roll.length; n ++){
+
+                if(roll[i-1] == i){
+                    numEqual ++;
+                }
+                if(numEqual == 3){
+                    return true;
+                }
+
+            }
+
+            numEqual = 0;
+        }
+        return false;
     }
 }

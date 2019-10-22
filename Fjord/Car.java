9b17715f8ae2632ec29e18057c2babcfd9a216bd
Ben McMann <bbmcmann@volosys.com>
2019-10-22 12:42:42 -0600

    Set Getters

--- Fjord/Car.java
+++ Fjord/Car.java
@@ -1,12 +1,31 @@
 public class Car{
 
+    private String make;
+    private String type;
+    private int tankSize;
+    private double time;
 
+    public Car(String make, String type, int tankSize, double time){
+        this.make = make;
+        this.type = type;
+        this.tankSize = tankSize;
+        this.time = time;
+    }
 
+    public String getMake(){
+        return this.make;
+    }
 
+    public String getType(){
+        return this.type;
+    }
 
-    public Car(){
+    public int getTankSize(){
+        return this.tankSize;
+    }
 
-
+    public double getTime(){
+        return this.time;
     }
 
 
